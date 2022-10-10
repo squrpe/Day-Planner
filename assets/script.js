95b957ccd438936c2935660100cd38530bb7b258
Lara Grocke <laragrocke@Laras-MacBook-Air.local>
2022-10-10 15:34:07 +1030

    initial commit 2

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    let dayEl = $('#currentDay');
+
+    dayEl.text(moment().format('dddd MMMM Do YYYY, h:mm:ss a'));
+
+        //var currentTime = moment();
+        //currentTime = currentTime.startOf("hour");
+
+});
